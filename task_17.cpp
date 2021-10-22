293a0198d0336ae012ca769ce0b2c7c5dc3d014f
AlexanderLukashuk <upiter465@gmail.com>
2021-10-22 20:35:30 +0600

    Добавленна функция IntelligenceService, план с сортировкой массива провалился

--- task_17.cpp
+++ task_17.cpp
@@ -2,7 +2,18 @@
 
 using namespace std;
 
+void IntelligenceService(int arr[], int size)
+{
+    int height_difference;
 
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            
+        }
+    }
+}
 
 int main()
 {
@@ -12,8 +23,10 @@ int main()
     
     int soldiers[size];
 
-    for (int i = 0; i << size; i++)
+    for (int i = 0; i < size; i++)
     {
         cin >> soldiers[i];
     }
+
+    IntelligenceService(soldiers, size);
 }
