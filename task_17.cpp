3212ab1af0bb7d982511918f856f335cc1b2e164
AlexanderLukashuk <upiter465@gmail.com>
2021-10-22 20:48:21 +0600

    Добавленны переменные height_difference, temp_difference, сделано условие нахождения минимальной разности в росте солдат

--- task_17.cpp
+++ task_17.cpp
@@ -4,23 +4,38 @@ using namespace std;
 
 void IntelligenceService(int arr[], int size)
 {
-    int height_difference;
+    int height_difference = 100;
+    int temp_difference = 100;
+    int indexI, indexJ;
 
     for (int i = 0; i < size - 1; i++)
     {
-        for (int j = 0; j < size - i - 1; j++)
+        for (int j = 0; j < size; j++)
         {
-            
+            if (arr[i] - arr[j] < temp_difference)
+            {
+                temp_difference = arr[i] - arr[j];
+                indexI = i;
+                indexJ = j;
+            }
         }
+
+        if (temp_difference < height_difference)
+        {
+            height_difference = temp_difference;
+        }
+        temp_difference = 0;
     }
+
+    cout << indexI << " " << indexJ << endl;
 }
 
 int main()
 {
     int size;
-    
+
     cin >> size;
-    
+
     int soldiers[size];
 
     for (int i = 0; i < size; i++)
